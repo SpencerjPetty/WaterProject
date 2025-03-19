d34863ce5534db72cbd1975eb6a140cf71b7a270
SpencerjPetty <141672620+SpencerjPetty@users.noreply.github.com>
2025-03-19 08:39:33 -0600

    Initial Commit

---
+++ backend/WaterProject.API/Controllers/WaterController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
+using WaterProject.API.Data;
+
+namespace WaterProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WaterController : ControllerBase
+    {
+        private readonly WaterDbContext _waterContext;
+        public WaterController(WaterDbContext temp) => _waterContext = temp;
+
+        [HttpGet("AllProjects")]
+        public IEnumerable<Project> GetProjects()
+        {
+            var projectList = _waterContext.Projects.ToList();
+
+            return projectList;
+        }
+
+    }
+}
